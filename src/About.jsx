06b643a8678f0ff18631d6309ed6b1940aeb7202
Aldwinray <aguiloaldwin@gmai.com>
2025-02-25 02:02:35 +0800

    Updated content in added-route

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactPic from "../src/ReactPic.jpg"; 
+import ReactPic from "../src/ReactPic.jpg";
 
 const About = () => {
     return (
         <div className="about">
             <div className="about-container">
                 <img src={ReactPic} alt="Aldwin Ray Aguilo" className="about-image" />
+                
                 <div className="about-content">
                     <h1>About Me</h1>
                     <p>
@@ -13,6 +14,24 @@ const About = () => {
                         problem-solving, and continuous learning. I love crafting engaging user experiences 
                         and exploring new technologies.
                     </p>
+                    <h2>Skills & Expertise</h2>
+                    <div className="skills-container">
+                        <span>HTML & CSS</span>
+                        <span>Arduino & IoT</span>
+                    </div>
+                    <h2>Career Goals</h2>
+                    <p>
+                        My goal is to become a skilled front-end developer, building 
+                        user-friendly applications while continuously learning new technologies.
+                    </p>
+                    <h2>Fun Facts & Hobbies</h2>
+                    <p>
+                        I enjoy playing drums and guitars, listening to music, exploring new tech, and traveling. 
+                        One of my dream trips is to visit Japan to experience the places I have seen while watching anime!
+                    </p>
+                    <h2>Let's Connect!</h2>
+                    <p>If you're interested in working together, feel free to reach out!</p>
+                    <p>aguiloaldwin03@gmail.com</p>
                 </div>
             </div>
         </div>
