7f01f4fcc42a3500316dd54ad4ae77a995dc9ee8
Aldwinray <aguiloaldwin@gmai.com>
2025-02-25 02:26:23 +0800

    updated content

--- src/About.jsx
+++ src/About.jsx
@@ -26,12 +26,12 @@ const About = () => {
                     </p>
                     <h2>Fun Facts & Hobbies</h2>
                     <p>
-                        I enjoy playing drums and guitars, listening to music, exploring new tech, and traveling. 
+                        I enjoy playing the drums, listening to music, exploring new tech, and traveling. 
                         One of my dream trips is to visit Japan to experience the places I have seen while watching anime!
                     </p>
                     <h2>Let's Connect!</h2>
                     <p>If you're interested in working together, feel free to reach out!</p>
-                    <p>aguiloaldwin03@gmail.com</p>
+                    <p>aguiloaldwin03@gmail.com or 09058803221</p>
                 </div>
             </div>
         </div>
